95283a576a8e960ffdd0ba44b878f68b77624d1d
suxxus <suxxus@outlook.com>
2017-04-18 05:48:30 +0200

    updated index with new exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const simpleCounter = require('./scripts/redux-implementation-simple-counter');
+const simpleCounterRealRedux = require('./scripts/redux-implementation-simple-counter-using-real-redux');
 
 module.exports = {
-    simpleCounter
+    simpleCounter,
+    simpleCounterRealRedux
 };

8bb25b5d5fb06bf9d443d27d9dccd9755872d2aa
suxxus <suxxus@outlook.com>
2017-04-22 16:02:35 +0200

    added module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const simpleCounter = require('./scripts/redux-implementation-simple-counter');
 const simpleCounterRealRedux = require('./scripts/redux-implementation-simple-counter-using-real-redux');
+const recipesBook = require('./scripts/recipes-book');
 
 module.exports = {
     simpleCounter,
-    simpleCounterRealRedux
+    simpleCounterRealRedux,
+    recipesBook
 };

f54cf8d852b19a52a291d779642e6455d7e4a4a0
suxxus <suxxus@outlook.com>
2017-05-17 12:51:14 +0200

    created server

---
+++ server/api.js
@@ -0,0 +1,38 @@
+/* eslint one-var: ["off"] */
+const path = require('path'),
+  fs = require('fs');
+
+const getJsonFromFile = file =>
+    fs.readFileSync(file, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      return JSON.parse(data);
+    });
+
+const api = function api(app, opts) {
+  app.get('/api/data', (req, res) => {
+    const file = path.format({
+      dir: opts.data,
+      base: 'data.json',
+    });
+
+    res
+            .status(200)
+            .send(getJsonFromFile(file));
+  });
+
+  app.post('/api/data', (req, res) => {
+    res.sendStatus(200);
+  });
+
+  app.delete('/api/data', (req, res) => {
+    res.sendStatus(200);
+  });
+
+  app.put('/api/data', (req, res) => {
+    res.sendStatus(200);
+  });
+};
+
+module.exports = api;
---
+++ server/index.js
@@ -0,0 +1,60 @@
+/* eslint no-console: ["off"] */
+/* eslint one-var: ["off"] */
+
+const bodyparser = require('body-parser'),
+    fs = require('fs'),
+    compression = require('compression'),
+    cors = require('cors'),
+    express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    socketio = require('socket.io');
+
+const port = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(compression());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// api
+const getJsonFromFile = file =>
+    fs.readFileSync(file, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        return JSON.parse(data);
+    });
+
+app.get('/api/recipes', (req, res) => {
+    const file = path.format({
+        dir: path.join(__dirname, 'data'),
+        base: 'recipes.json',
+    });
+
+    console.log(file);
+    res
+        .status(200)
+        .send(getJsonFromFile(file));
+});
+//};
+
+// IO
+// io.on('connect', function(socket) {
+//     socket.on('name', data => {
+//         socket.emit('greet', `hello ${data}`);
+//     });
+
+//     socket.on('resp', data => {
+//         socket.emit('helloAgain', `${data}`);
+//     });
+// });
+
+server.listen(8080, () => {
+    const host = server.address().address;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
